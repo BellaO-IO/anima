9a64ede7e1d8bed3eee6c852a202cc39dffb35d4
bella <theboss@ciaobella.me>
2021-06-11 11:04:32 -0700

    first commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+            Console.WriteLine($"Once upon a time there was a {animal} named {name} it was pretty cool, it was {color}. It was also in a band named {band}, as the head singer.");
+
+            
+            
+
+
+
+
+
+
+        }
+    }
+}
